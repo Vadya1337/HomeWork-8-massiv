f697bd255a60e50ef659803ac09c0c273380c2fd
Вадим <vadim.filatov.02@gmail.com>
2024-09-10 01:04:53 +0700

    Сделал дз

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,58 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // задание 1 и 2
+        {
+            int[] weights = new int[3];
+            weights[0] = 1;
+            weights[1] = 2;
+            weights[2] = 3;
+            System.out.println(Arrays.toString(weights));
+            double[] b = new double[3];
+            b[0] = 1.57d;
+            b[1] = 7.654d;
+            b[2] = 9.986d;
+            System.out.println(Arrays.toString(b));
+            int[] c = new int[]{9, 10, 11, 12, 13};
+            System.out.println(Arrays.toString(c));
+        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        {
+            System.out.println();
+            // Задание 3
+            {
+                int[] weights = new int[3];
+                weights[0] = 1;
+                weights[1] = 2;
+                weights[2] = 3;
+
+                for (int i = weights.length - 1; i >= 0; i--) {
+                    System.out.printf(", " + weights[i]);
+                }
+                System.out.println();
+                double[] b = new double[3];
+                b[0] = 1.57d;
+                b[1] = 7.654d;
+                b[2] = 9.986d;
+                for (int a = b.length - 1; a >= 0; a--) {
+                    System.out.printf("," + b[a]);
+                }
+                System.out.println();
+                int[] c = new int[]{9, 10, 11, 12, 13};
+                for (int g = c.length - 1; g >= 0; g--) {
+                    System.out.printf("," + c[g]);
+                }
+            }
+            // Задание 4
+            System.out.println();
+            int[] weights = new int[3];
+            weights[0] = 1 + 1;
+            weights[1] = 2;
+            weights[2] = 3 + 1;
+            System.out.println(Arrays.toString(weights));
         }
     }
 }
